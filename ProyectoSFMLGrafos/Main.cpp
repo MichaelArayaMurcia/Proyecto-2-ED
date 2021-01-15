84dcf404da72e8022dd5d61fdd4e4908a0c85f25
JoséAraya <arayacastilloj@gmail.com>
2021-01-15 15:35:50 -0600

    despiche d=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)bd=====(￣▽￣*)

--- ProyectoSFMLGrafos/Main.cpp
+++ ProyectoSFMLGrafos/Main.cpp
@@ -73,10 +73,12 @@ int main(){
     sf::RectangleShape shape2(sf::Vector2f(10, 10));
     sf::RectangleShape shape3(sf::Vector2f(10, 10));
     sf::RectangleShape shape4(sf::Vector2f(10, 10));
+    sf::RectangleShape shape5(sf::Vector2f(10, 10));
     shape.setFillColor(sf::Color::Magenta);
     shape2.setFillColor(sf::Color::Green);
     shape3.setFillColor(sf::Color::Red);
     shape4.setFillColor(sf::Color::Blue);
+    shape5.setFillColor(sf::Color::White);
 
     float rotacion = 0;
     float rotacion2 = 90;
@@ -84,6 +86,8 @@ int main(){
     float rotacion4 = 270;
     float x_coord = 0;
     float y_coord = 0;
+    int dibujarLaberinto = 0;
+    int cambiarDireccion = 1;
     while (window.isOpen())
     {
         sf::Event event;
@@ -92,11 +96,30 @@ int main(){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear();
         //window.draw(shape);
         //window.draw(shape2);
         window.draw(shape3);
         window.draw(shape4);
+        if (dibujarLaberinto%1==0) {
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 100; j++) {
+                    window.draw(shape5);
+                    if(j%2 != 0)
+                    shape5.setPosition(j * 30+(dibujarLaberinto), i * 10);
+                    else
+                    shape5.setPosition(j * 35-(dibujarLaberinto), i * 30);
+                    
+                }
+            }
+        }
+        dibujarLaberinto+=cambiarDireccion;
+        
+        if (dibujarLaberinto % 100 == 0)
+            cambiarDireccion *= -1;
+        
         window.display();
+
         shape2.setRotation(rotacion);
         shape.setRotation(rotacion2);
         //shape3.setRotation(rotacion3);
@@ -109,17 +132,18 @@ int main(){
         shape.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
         shape3.setPosition(x_coord, y_coord);
         shape4.setPosition(x_coord+1, y_coord+1);
+        
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            x_coord += 0.2;
+            x_coord += 2;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            x_coord -= 0.2;
+            x_coord -= 2;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            y_coord -= 0.2;
+            y_coord -= 2;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            y_coord += 0.2;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            window.clear();
+            y_coord += 2;
+        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+            
     }*/
 
 
